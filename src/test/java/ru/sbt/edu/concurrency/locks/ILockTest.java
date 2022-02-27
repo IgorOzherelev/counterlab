f54ff1646cd9caa6a841c87149269deaa1d417b1
IgorOzherelev <i9163073959@gmail.com>
2022-02-27 17:48:04 +0300

    Реализованы блокироващики

--- src/test/java/ru/sbt/edu/concurrency/locks/ILockTest.java
+++ src/test/java/ru/sbt/edu/concurrency/locks/ILockTest.java
@@ -4,19 +4,20 @@ import org.junit.Test;
 import ru.sbt.edu.concurrency.counter.Counter;
 import ru.sbt.edu.concurrency.counter.ILockCounter;
 import ru.sbt.edu.concurrency.counter.SeqCounter;
-import ru.sbt.edu.concurrency.locks.theory.LockTwo;
-import ru.sbt.edu.concurrency.locks.theory.LockZero;
+import ru.sbt.edu.concurrency.locks.theory.LockOne;
+import ru.sbt.edu.concurrency.locks.theory.PetersonLock;
 import ru.sbt.edu.concurrency.util.TwoThreadIds;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class ILockTest {
+
     @Test
-    public void testTheoryLock()  {
-        ILock lock = new LockZero();
+    public void testPetersonLock()  {
+        ILock lock = new LockOne();
         Counter counter = new ILockCounter(lock);
         //try: 1, 2, 10, 100, 1000
-        testCounter(counter, 1);
+        testCounter(counter, 100);
     }
 
     @Test
@@ -26,9 +27,9 @@ public class ILockTest {
         testCounter(counter, 1000);
     }
 
-    private void testCounter(Counter counter, int iters) {
+    private void testCounter(Counter counter, long iters) {
         Runnable increment = () -> {
-            System.out.println(TwoThreadIds.me());
+            System.out.println("threadId: " + TwoThreadIds.me());
             for (int i = 0; i < iters; i++) {
                 counter.increment();
             }
@@ -47,7 +48,7 @@ public class ILockTest {
         }
 
         long count = counter.getValue();
-        System.out.println(count);
+        System.out.println("Counter value: " + count);
         assertEquals("Oops! Unexpected Behaviour!", iters * 2, count);
     }
 }
