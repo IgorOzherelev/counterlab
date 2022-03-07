674c8e0d4932a52743fac41d41a7a17420b4eb2a
IgorOzherelev <i9163073959@gmail.com>
2022-03-07 12:06:07 +0300

    fairness semaphore + concurrent map

--- src/main/java/ru/sbt/edu/concurrency/counter/ConcurrentCounter.java
+++ src/main/java/ru/sbt/edu/concurrency/counter/ConcurrentCounter.java
@@ -1,16 +1,20 @@
 package ru.sbt.edu.concurrency.counter;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
 
 public class ConcurrentCounter implements Counter {
     private long counter = 0;
-    private final Semaphore semaphore = new Semaphore(1, true);
+    private final ConcurrentMap<String, Long> counterMap = new ConcurrentHashMap<>();
+    private final Semaphore semaphore = new Semaphore(1, true); // обеспечен fairness
 
     @Override
     public void increment() {
         try {
             semaphore.acquire();
             counter++;
+            counterMap.put("counter", counter);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -20,6 +24,6 @@ public class ConcurrentCounter implements Counter {
 
     @Override
     public long getValue() {
-        return counter;
+        return counterMap.get("counter");
     }
 }
